4f3d9b5ab4db0bcff4074fe9657378f4f53926e8
zhenghao <iron4wine@outlook.com>
2018-05-13 11:49:24 -0700

    update

--- app.js
+++ app.js
@@ -8,3 +8,14 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+let scores, roundScore, activePlay, dice;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+dice = Math.floor(Math.random()*6)+1; // random number between 0 - 6
+console.log(dice);
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
