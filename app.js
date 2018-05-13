30f7080553658f4681f0b9abda4068fd1261e0fb
zhenghao <iron4wine@outlook.com>
2018-05-13 12:24:08 -0700

    update

--- app.js
+++ app.js
@@ -9,12 +9,29 @@ GAME RULES:
 
 */
 
-let scores, roundScore, activePlay, dice;
+var scores, roundScore, activePlay;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
-dice = Math.floor(Math.random()*6)+1; // random number between 0 - 6
-console.log(dice);
 
-document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+
+
+document.querySelector('.btn-roll').addEventListener('click',function() {
+  var dice = Math.floor(Math.random()*6)+1;  // random number between 0 - 6
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'dice-' + dice + '.png';
+
+  if (dice !== 1) {
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  } else {
+    activePlayer === 0 ? activePlayer = 1: activePlayer = 0;
+  }
+});
