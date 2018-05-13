dd64673d3aa97b89d171ea11af7c05ba35a53bb1
zhenghao <iron4wine@outlook.com>
2018-05-13 13:20:05 -0700

    update

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
   if (gamePlaying) {
     scores[activePlayer] += roundScore;
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-    if (scores[activePlayer]>=10) {
+    if (scores[activePlayer]>=100) {
       document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
       document.querySelector('.dice').style.display = 'none';
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
