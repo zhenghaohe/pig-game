81d6dab780ddfe503fc85723dcf100ce6e88bec1
zhenghao <iron4wine@outlook.com>
2018-05-13 11:50:19 -0700

    update

--- app.js
+++ app.js
@@ -17,5 +17,4 @@ activePlayer = 0;
 dice = Math.floor(Math.random()*6)+1; // random number between 0 - 6
 console.log(dice);
 
-//document.querySelector('#current-' + activePlayer).textContent = dice;
 document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
