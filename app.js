a479d44fc06e5b313813058e18442390ecb347cd
zhenghao <iron4wine@outlook.com>
2018-05-13 12:46:50 -0700

    update

--- app.js
+++ app.js
@@ -32,12 +32,32 @@ document.querySelector('.btn-roll').addEventListener('click',function() {
     roundScore += dice;
     document.querySelector('#current-' + activePlayer).textContent = roundScore;
   } else {
-    roundScore = 0;
-    activePlayer === 0 ? activePlayer = 1: activePlayer = 0;
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
-    document.querySelector('.dice').style.display = 'none';
+    nextPlayer();
   }
 });
+
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+  scores[activePlayer] += roundScore;
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+  if (scores[activePlayer]>=10) {
+    document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+  }else{
+    nextPlayer();
+  }
+
+
+});
+
+function nextPlayer() {
+  roundScore = 0;
+  activePlayer === 0 ? activePlayer = 1: activePlayer = 0;
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+  document.querySelector('.dice').style.display = 'none';
+}
